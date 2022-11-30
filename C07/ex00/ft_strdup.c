aac46b413fae1b4064901d7698f4ea754aeed1eb
Will's Lullaby <100817004+dieremy@users.noreply.github.com>
2022-11-30 16:45:52 +0000

    Add files via upload

---
+++ C07/ex00/ft_strdup.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: robegarc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/25 14:17:11 by robegarc          #+#    #+#             */
+/*   Updated: 2022/10/25 16:45:52 by robegarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char *array;
+	int	i;
+	int	len;
+
+	len = ft_strlen(src) - 1;
+	array = (char*)malloc(sizeof(char) * (len + 1));
+	i = 0;
+	while (i <= len)
+	{
+		array[i] = src[i];
+		i++;
+	}
+	array[i] = '\0';
+	return (array);
+}
