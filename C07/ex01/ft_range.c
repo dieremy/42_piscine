bd881e699f62e58a7697add254e5fcb41481e7b9
Will's Lullaby <100817004+dieremy@users.noreply.github.com>
2022-12-02 19:49:15 +0000

    Update ft_range.c

--- C07/ex01/ft_range.c
+++ C07/ex01/ft_range.c
@@ -11,31 +11,24 @@
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
 
 int	*ft_range(int min, int max)
 {
 	int	*array;
+	int	size;
 	int	i;
 
 	if (min >= max)
 		return (0);
-	array = (int*)malloc(sizeof(int) * (max - min));
+	size = max - min;
+	array = (int*)malloc(sizeof(int) * size);
 	if (!array)
 		return (0);
 	i = 0;
-	while (i < (max - min))
+	while (i < range)
 	{
 		array[i] = min++;
 		i++;
 	}
-	//array[i] = '\0';
 	return (array);
-}
-
-int main()
-{
-	int *a = ft_range(13, 39);
-	for (int i = 0; i < 26; i++)
-		printf("%d\n", a[i]);
 }
